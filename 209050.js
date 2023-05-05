4ef8dbd50a741f26a8ed5bdc1436f168d59119b7
Sven Gatnar <sgatnar@stud.hs-heilbronn.de>
2023-05-05 17:16:43 +0200

    alles oder nichts

---
+++ 209050.js
@@ -0,0 +1,6 @@
+Debug.log("Hello World");
+
+//fetch('http://127.0.0.1:5500/PersonData.json')
+//  .then(response => response.json())
+//  .then(data => console.log(data))
+//  .catch(error => console.error(error));
