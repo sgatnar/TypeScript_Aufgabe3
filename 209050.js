033dccefea488ef67482f9ddacefe427e1ff3592
Sven Gatnar <sgatnar@stud.hs-heilbronn.de>
2023-05-06 20:04:46 +0200

    Created class ArrtibuteFetcher

--- 209050.js
+++ 209050.js
@@ -1,11 +1,26 @@
-    
-function getAttributes(){
-    return fetch('http://localhost:5500/PersonData.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.name); // do something with the data here
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
+class AttributeFetcher {
+  constructor() {
+    this.fetchDataButton = document.getElementById("fetch-data-button");
+    this.addClickListener();
+  }
+
+  addClickListener() {
+    this.fetchDataButton.addEventListener("click", () => {
+      console.log("It is working");
+      //this.getAttributes();
     });
+  }
+
+  getAttributes() {
+    return fetch('http://127.0.0.1:5500/PersonData.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // do something with the data here
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
 }
+
+const attributeFetcher = new AttributeFetcher();
